e59369a18442b9ab149bb59fd743b34bfa5a07fa
Olivier Levitt <olivier.levitt@insee.fr>
2019-12-20 13:52:22 +0100

    Fix openapi location

--- web/src/main/java/fr/insee/innovation/marathontokubernetes/api/MarathonToKubernetesApiApplication.java
+++ web/src/main/java/fr/insee/innovation/marathontokubernetes/api/MarathonToKubernetesApiApplication.java
@@ -2,14 +2,6 @@ package fr.insee.innovation.marathontokubernetes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication(scanBasePackages = "fr.insee.innovation.marathontokubernetes")
 public class MarathonToKubernetesApiApplication {
--- web/src/main/java/fr/insee/innovation/marathontokubernetes/api/configuration/OpenAPIConfiguration.java
+++ web/src/main/java/fr/insee/innovation/marathontokubernetes/api/configuration/OpenAPIConfiguration.java
@@ -1,18 +1,13 @@
 package fr.insee.innovation.marathontokubernetes.api.configuration;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfiguration {
 
-    @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("Marathon to Kubernetes API")
