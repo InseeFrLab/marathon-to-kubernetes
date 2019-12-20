8a6be597f3c09c9b627a6f600e20ee09d3965aa1
Olivier Levitt <olivier.levitt@insee.fr>
2019-12-20 13:23:44 +0100

    Add basic test

--- web/src/main/java/fr/insee/innovation/marathontokubernetes/api/controller/ApiConverterController.java
+++ web/src/main/java/fr/insee/innovation/marathontokubernetes/api/controller/ApiConverterController.java
@@ -1,28 +1,20 @@
 package fr.insee.innovation.marathontokubernetes.api.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import fr.insee.innovation.marathontokubernetes.core.services.converter.MarathonToKubernetesConverter;
 import fr.insee.innovation.marathontokubernetes.core.services.kubernetes.YmlExporter;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import mesosphere.marathon.client.model.v2.App;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
-public class ApiConverter {
+public class ApiConverterController {
 
     @Autowired
     YmlExporter exporter;
